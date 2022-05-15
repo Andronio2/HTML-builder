0e4312ef81debcc4fe97099e8a1538e5f5d3b4bf
Andronio <gprsboost03@mail.ru>
2022-05-15 17:38:40 +0600

    feat: done 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const fname = path.join(__dirname, 'secret-folder');
+const fs = require('fs');
+
+fs.readdir(fname, {withFileTypes: true}, (err, files) => {
+  files.forEach(file => {
+    if (file.isFile()) {
+      fs.stat(path.join(fname, file.name), (err, stats) => {
+        const fileinfo = path.parse(file.name);
+        const size = (stats.size <= 1024) ? stats.size + ' B' : (Math.floor(stats.size / 10) / 100) + ' kB';
+        console.log(fileinfo.name + ' - ' + fileinfo.ext.slice(1) + ' - ' + size);
+      });
+    }
+  });
+});
