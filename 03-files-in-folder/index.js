e279434d43d2163eec79f6c45a5bb80aee8b4a11
Andronio <gprsboost03@mail.ru>
2022-05-15 17:42:38 +0600

    fix: 03-files-in-folder 3 number after dot in kB

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -7,7 +7,7 @@ fs.readdir(fname, {withFileTypes: true}, (err, files) => {
     if (file.isFile()) {
       fs.stat(path.join(fname, file.name), (err, stats) => {
         const fileinfo = path.parse(file.name);
-        const size = (stats.size <= 1024) ? stats.size + ' B' : (Math.floor(stats.size / 10) / 100) + ' kB';
+        const size = (stats.size <= 1024) ? stats.size + ' B' : (stats.size / 1000) + ' kB';
         console.log(fileinfo.name + ' - ' + fileinfo.ext.slice(1) + ' - ' + size);
       });
     }
